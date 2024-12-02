2518bb4ca2e07c3ed6513275d437fe024090761b
metz <daryljmetzler@gmail.com>
2024-12-01 21:25:16 -0800

    2 a

---
+++ 2/a.jl
@@ -0,0 +1,37 @@
+using IterTools
+
+function readfile(filename::String)
+  @show filename
+  lines = readlines(filename)
+  return lines
+end
+
+function parseInput(lines)
+  splitAndCast = line -> map(n -> parse(Int, n), split(line))
+  return map(splitAndCast, lines)
+end
+
+function safe(report)
+  @show report
+  pairs = IterTools.partition(report, 2, 1)
+  for (i, j) in pairs
+    if abs(j - i) > 3 || i == j
+      return false
+    end
+  end
+  monotonic_inc = p -> p[1] < p[2]
+  monotonic_dec = p -> p[1] > p[2]
+  return all(monotonic_inc, pairs) || all(monotonic_dec, pairs)
+end
+
+function main(args)
+    if length(args) != 1
+      throw("no input!")
+    end
+
+    filename = args[1]
+    lines = readfile(filename)
+    reports = parseInput(lines)
+    @show count(safe, reports)
+end
+main(ARGS)
